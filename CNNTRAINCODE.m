6a3a6471029816c93350b3ff717da3baa1117214
123456789khalid <93932137+123456789khalid@users.noreply.github.com>
2025-08-15 18:23:08 +0800

    Add files via upload

---
+++ CNNTRAINCODE.m
@@ -0,0 +1,55 @@
+
+imds = imageDatastore('C:\Users\HP\Desktop\Khalid Code\Khalid updated Dataset\Class of final dataset','IncludeSubfolders',true,'LabelSource','foldernames');
+
+[imdsTrain,imdsValidation] = splitEachLabel(imds,0.7,'randomized');
+
+%%
+% Load the Transfer Learning CNN Model
+ load('C:\Users\HP\Desktop\Khalid Code\Khalid updated Dataset/khalidCNN');
+ 
+%%
+
+%Select the Test images and save in Y_test
+Y_test = imdsValidation.Labels;
+
+%%
+
+% optimzation techniques selection and hyperparamter selection
+option = trainingOptions('adam', ...
+    'MiniBatchSize',16, ...
+    'MaxEpochs',10, ...
+    'InitialLearnRate',1e-4, ...
+    'Shuffle','every-epoch', ...
+    'ValidationData',imdsValidation, ...
+    'ValidationFrequency',3, ...
+    'Verbose',false, ...
+    'Plots','training-progress');
+
+%%
+%CNN model training
+netTransfer = trainNetwork(imdsTrain,waleedCNN2,option);
+
+
+
+%%
+% CNN Model validation
+for i=1:numel(imdsValidation.Files)
+     a=[imdsValidation.Files(i)];
+     a = imread(char(a));
+    YPred(i,1) = classify(netTransfer,a);
+    i
+ 
+ 
+end
+%%
+YPred = classify(netTransfer,imdsValidation);
+
+%%
+%Performance evaluation of Deep Learning Trained Model
+plotconfusion(Y_test,YPred),title('Confusion Matrix');
+%%
+results= Evaluate(Y_test,YPred)
+accuracy = results(1)
+precision = results(4)
+recall = results(5)
+f_measure = results(6)
